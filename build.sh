8479b0d42a863e1a3eb36a6ce15f17d2d0067b90
Martin Joehren <martin.joehren@esailors.de>
2016-01-12 11:57:36 +0000

    use correct git hash and date

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -e -o pipefail
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/go/src/app -e DOCKER_IMAGE=matlockx/datadog-metrics-tuner matlockx/golang-builder -ldflags "-X=main.buildTime=2016-01-12_11:17:04AM -X=main.gitHash=80cb93c613cf1d5db4cc6c44e2ef27c4636a066a" -o datadog-metrics-tuner
+if [ -n "$(git status -s)" ]; then
+	echo "Your working directory isn't clean. Commit or stash your changes and continue."
+	exit 1
+fi
+
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/go/src/app -e DOCKER_IMAGE=matlockx/datadog-metrics-tuner matlockx/golang-builder -ldflags "-X=main.buildTime=$(date -u '+%Y-%m-%d_%I:%M:%S%p') -X=main.gitHash=$(git rev-parse HEAD)" -o datadog-metrics-tuner
 
 docker push matlockx/datadog-metrics-tuner
